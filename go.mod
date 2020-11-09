4253f58d2205fa494cc620b5d1c70c1498f70d9b
Orlov Konstantin <orlov.k@citilink.ru>
2020-11-09 19:46:42 +0300

    commit добавил обработку файла

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ImgSize
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/collinux/watermark v0.0.0-20180119182120-85699970d2cd
+	github.com/gorilla/mux v1.8.0
+)
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/collinux/watermark"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Article struct {
@@ -19,70 +21,42 @@ type Option struct {
 	Message string `json:"Message"`
 }
 
-//Articles - local DataBase
-var Articles []Article
-
 //GET request for /articles
 func GetAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hint: getAllArticles woked.....")
-	text := "встав рядом ссылкой http://localhost:8000/articles/ ='{сыслка нас скачивыемый снимок}'"
-	json.NewEncoder(w).Encode(text)
-}
+	fmt.Println("Hint: getAllArticles worked.....")
+	files, err := ioutil.ReadDir("download/")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-const lenPath = len("/download/")
+	for _, file := range files {
+		logo := watermark.Watermark{Source: "logoPic/g.png"}
+		logo.Apply("download/" + file.Name())
+		fmt.Println(logo)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 //GET request for article with ID
 func GetArticleWithId(w http.ResponseWriter, r *http.Request) {
-	name := "<form method='POST' action='/add'>URL: <input type='text' name='url'><input type='submit' value='Add'></form>"
-	fmt.Fprintf(w, name)
-
-	//if len(os.Args) != 3 {
-	//	fmt.Println(vars)
-	//	os.Exit(1)
-	//}
-	//url := os.Args[1]
-	//filename := os.Args[2]
-	//
-	//err := DownloadFile(url, filename)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//if !find {
-	//	var erM = ErrorMessage{Message: "Not found article with that ID"}
-	//	json.NewEncoder(w).Encode(erM)
-	//}
+	fmt.Println("I did it")
+	//reqBody, _, err := image.Decode(r.Body)
+	reqBody1, err := ioutil.ReadAll(r.Body)
+	file, err := os.Open(reqBody1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(reqBody1)
 }
-
-//func DownloadFile(url string, filepath string) error {
-//	// Create the file
-//	out, err := os.Create(filepath)
-//	if err != nil {
-//		return err
-//	}
-//	defer out.Close()
-//
-//	// Get the data
-//	resp, err := http.Get(url)
-//	if err != nil {
-//		return err
-//	}
-//	defer resp.Body.Close()
-//
-//	// Write the body to file
-//	_, err = io.Copy(out, resp.Body)
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
 
 func main() {
 	fmt.Println("REST API V2.0 worked....")
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/articles", GetAllArticles).Methods("GET")
-	myRouter.HandleFunc("/download/", GetArticleWithId).Methods("GET")
+	myRouter.HandleFunc("/download", GetAllArticles).Methods("GET")
+	myRouter.HandleFunc("/logo", GetArticleWithId).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
